fb4db6465ec97d0e42d51ab9be50705c4450dffc
YURADEAD <90732098+YURADEAD@users.noreply.github.com>
2023-11-04 01:10:11 -0400

    Recommiting fixed (was broken) App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { LessonsPage } from './components/Lesson/LessonsList';
+import Lessons from "./pages/Lessons/Lessons"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const topic1 = {
+  name: "topic1",
+  lessons: ["leson1", "lesson2", "lesson3"]
+};
+
+const topic2 = {
+  name: "topic2",
+  lessons: ["leson1", "lesson2", "lesson3"]
+};
+
+const tempTopics = [topic1, topic2];
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <LessonsPage/>
+      <div>
+        <NavBar/>
+        <body>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/Lessons" element={<Lessons topics = {tempTopics}/>}/>
+        </Routes>
+        </BrowserRouter>
+        </body>
+      </div>
     </>
   )
 }
