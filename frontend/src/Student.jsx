4c40309b58ac63ca83bd77cba3350afc1a6c0a9a
Zakariya Ahmed <zakariyaahmed077@gmail.com>
2023-11-04 00:37:46 -0400

    Implemented Curriculum Cards

--- frontend/src/Student.jsx
+++ frontend/src/Student.jsx
@@ -4,28 +4,24 @@ import './Student.css'; // Import a CSS file for styling
 
 const Student = () => {
     return (
-        <div className="student-roadmap">
-            <div className="roadmap-card">
-                <h2>Step 1</h2>
-                <p>This is the first step in your roadmap.</p>
-                <div className="arrow-down">
-                  <a href="#step2">Step 2</a>
+        <div className="main-con">
+            <div className="road-map">
+            <div className="card-container">
+                <div className="card">
+                    <h1>Topic 1</h1>
                 </div>
             </div>
-            <div className="roadmap-card">
-                <h2 id="step2">Step 2</h2>
-                <p>This is the second step in your roadmap.</p>
-                <div className="arrow-down">
-                  <a href="#step3">Step 3</a>
+            <div className="card-container">
+                <div className="card">
+                    <h1>Topic 2</h1>
                 </div>
             </div>
-            <div className="roadmap-card">
-                <h2 id="step3">Step 3</h2>
-                <p>This is the third step in your roadmap.</p>
-                <div className="arrow-down">
-                  <a href="#step1">Step 1</a>
+            <div className="card-container">
+                <div className="card">
+                    <h1>Topic 3</h1>
                 </div>
             </div>
+        </div>
         </div>
     );
 }
