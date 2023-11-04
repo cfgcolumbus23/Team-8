657c67abaa2e51ed51b50365c372e2bfe9846d3f
YURADEAD <90732098+YURADEAD@users.noreply.github.com>
2023-11-04 01:03:06 -0400

    Updated dev with lessons feature

--- frontend/src/components/NavBar/LoginRegister.jsx
+++ frontend/src/components/NavBar/LoginRegister.jsx
@@ -4,15 +4,15 @@ const LoginRegister = (props) => {
     if(!props.auth) {
         return (
             <>
-                <Nav.Link href="#Login">Login</Nav.Link>
-                <Nav.Link href="#Register">Register</Nav.Link>
+                <Nav.Link href="Login">Login</Nav.Link>
+                <Nav.Link href="Register">Register</Nav.Link>
             </>
         );
     } else {
         return (
             <>
-                <Nav.Link href="#Profile">Profile</Nav.Link>
-                <Nav.Link href="#Dashboard">Sign Out</Nav.Link>
+                <Nav.Link href="Profile">Profile</Nav.Link>
+                <Nav.Link href="Dashboard">Sign Out</Nav.Link>
             </>
         );
     }
